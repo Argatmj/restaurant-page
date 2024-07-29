863f69327ebc063efbd7cfaa959abcdf38ac6e60
Argatmj <ikomangarga0612@gmail.com>
2024-07-29 16:39:28 +0200

    Added nav functionality

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@ import createHome from "./modules/home";
 import createMenu from "./modules/menu"
 import createContact from './modules/Contact';
 
-createContact();
+const home = document.querySelector("#Home");
+const menu = document.querySelector("#Menu");
+const contact = document.querySelector("#Contact");  
+const content = document.getElementById("content"); 
+
+document.addEventListener("DOMContentLoaded", createHome());
+
+home.addEventListener("click", () => {
+    content.textContent = '';
+    content.className = '';
+    createHome();
+})
+
+menu.addEventListener("click", () => {
+    content.textContent = '';
+    content.className = '';
+    createMenu();
+})
+
+contact.addEventListener("click", () => {
+    content.textContent = '';
+    content.className = '';
+    createContact();
+})
+
+
+
